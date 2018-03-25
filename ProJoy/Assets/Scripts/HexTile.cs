d58c0495b3494b1563dbf6cc58976f79e49a1135
Andras Horvath <andras@home.com>
2018-03-25 11:54:16 +0200

    Added HexTile and Map

---
+++ ProJoy/Assets/Scripts/HexTile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexTile {
+    
+    private GameObject hexagon, hexagonBorder;
+    
+    private static float height = 2.30862f;
+    private static float width = Mathf.Sqrt(3) / 2 * height;
+
+    //cubic coordinates
+    private int q, r, s;
+    
+	public HexTile(int col, int row) {
+        q = col;
+        r = row;
+        s = -q - r;        
+	}
+
+    public void Create(GameObject HexagonPrefab, GameObject HexagonBorderPrefab, Map map)
+    {
+        Vector2 pos =  new Vector2(width*(q+r/2f), height*r*0.75f);
+        hexagon = Object.Instantiate(HexagonPrefab, pos, Quaternion.identity, map.transform);
+        hexagonBorder = Object.Instantiate(HexagonBorderPrefab, pos, Quaternion.identity, map.transform);
+    }
+
+
+	
+}
---
+++ ProJoy/Assets/Scripts/Map.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour {
+
+    public GameObject HexagonPrefab;
+    public GameObject HexagonBorderPrefab;
+
+    HexTile[,] map;
+
+    int width = 15;
+    int height = 15;
+    
+	void Start () {
+        map = new HexTile[width, height];
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j< height; j++)
+            {
+                HexTile h = new HexTile(i, j);
+                map[i, j] = h;
+                h.Create(HexagonPrefab, HexagonBorderPrefab, this);
+            }
+        }
+
+	}
+	
+	void Update () {
+		
+	}
+}
